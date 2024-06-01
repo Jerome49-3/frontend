d9530c30da08b906e573ba58ed7e8f6dfda067e8
jerome <151376824+Jerome49-3@users.noreply.github.com>
2024-06-01 12:14:16 +0200

    premier commit

---
+++ assets/lib/index.js
@@ -0,0 +1,17 @@
+const doc = document;
+
+doc.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded");
+  doc.querySelector("#form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const response = await axios.post("/send-mail", {
+      fistname: doc.querySelector("firstname").value,
+      lastname: doc.querySelector("lastname").value,
+      email: doc.querySelector("email").value,
+      subject: doc.querySelector("subject").value,
+      message: doc.querySelector("message").value,
+    });
+    console.log("response.data:", response.data);
+    alert("email envoyé");
+  });
+});
