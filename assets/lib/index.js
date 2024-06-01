81fb82c8fd8c4238e78004227e19e6a4cf72b039
jerome <151376824+Jerome49-3@users.noreply.github.com>
2024-06-01 17:16:33 +0200

    add gitignore supp node_modules

--- assets/lib/index.js
+++ assets/lib/index.js
@@ -4,13 +4,16 @@ doc.addEventListener("DOMContentLoaded", () => {
   console.log("document loaded");
   doc.querySelector("#form").addEventListener("submit", async (event) => {
     event.preventDefault();
-    const response = await axios.post("/send-mail", {
-      fistname: doc.querySelector("firstname").value,
-      lastname: doc.querySelector("lastname").value,
-      email: doc.querySelector("email").value,
-      subject: doc.querySelector("subject").value,
-      message: doc.querySelector("message").value,
-    });
+    const response = await axios.post(
+      "https://site--backendsendmail--s4qnmrl7fg46.code.run/send-mail",
+      {
+        fistname: doc.querySelector("firstname").value,
+        lastname: doc.querySelector("lastname").value,
+        email: doc.querySelector("email").value,
+        subject: doc.querySelector("subject").value,
+        message: doc.querySelector("message").value,
+      }
+    );
     console.log("response.data:", response.data);
     alert("email envoyé");
   });
